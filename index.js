a3f18f27b54a8903d6c80f31f410ea6596fffafb
xmartin46 <xmartin46@gmail.com>
2020-01-29 21:41:05 +0200

    Corners now fine

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var buttonStart = document.getElementById('buttonStart');
+var buttonStop = document.getElementById('buttonStop');
 var buttonRandom = document.getElementById('buttonRandom');
 var gridOn = document.getElementById('gridOn');
 var fpsRange = document.getElementById('fps');
@@ -11,27 +12,39 @@ let height = 900;
 canvas.height = height;
 canvas.width = width;
 
-let nRows = 100;
+let nRows = 300;
 let nCols = nRows;
 
 var fps = 10;
 
+var animationID;
+var timeoutID;
+
 canvas.addEventListener("mousedown", getPosition, false);
+
 buttonStart.addEventListener('click', () => {
-  window.requestAnimationFrame(gameLoop);
+  start();
 })
+
+buttonStop.addEventListener('click', () => {
+  stop();
+})
+
 buttonRandom.addEventListener('click', () => {
   for (var i = 0; i < array.length; i++) {
     for (var j = 0; j < array[i].length; j++) {
-      array[i][j] = Math.random() > 0.5;
+      array[i][j] = Math.random() > 0.85;
     }
   }
 
+  drawGrid();
   drawArray(array);
 })
+
 gridOn.addEventListener('click', () => {
   drawGrid();
 })
+
 fpsRange.addEventListener('mouseup', function() {
     fps = this.value;
 });
@@ -47,7 +60,9 @@ function create2DArray(width, height) {
 
   return array
 }
+
 function drawGrid(){
+  if (gridOn.checked) {
     var mX = 0;
     var mY = 0;
     for (var x = 0; x <= width; x += width/nCols) {
@@ -61,12 +76,15 @@ function drawGrid(){
         context.lineTo(width, mY + x);
         context.lineWidth = 1;
     }
-
-    if (gridOn.checked) context.strokeStyle = "black";
-    else context.strokeStyle = "white";
+    context.strokeStyle = "black";
 
     context.stroke();
+  } else {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawArray(array);
+  }
 }
+
 function getPosition(event) {
   var x = event.x - canvas.offsetTop;
   var y = event.y - canvas.offsetTop;
@@ -81,6 +99,43 @@ function getPosition(event) {
     context.fillRect(Math.floor((x/width) * nCols) * width/nCols + 0.5, Math.floor((y/height) * nRows) * height/nRows + 0.5, width/nCols - 1.5, height/nRows - 1.5);
   }
 }
+
+function increaseArray(array) {
+  cArray = [];
+
+  // Initialize plus array values
+  cArray[0] = [];
+  cArray[array.length + 1] = [];
+
+  // Real values of matrix
+  for (var i = 0; i < array.length; i++) {
+        cArray[i + 1] = []
+    for (var j = 0; j < array[0].length; j++) {
+        cArray[i + 1][j + 1] = array[i][j];
+    }
+  }
+
+  // Corners
+  cArray[0][0] = array[array.length - 1][array[0].length - 1];
+  cArray[0][array[0].length + 1] = array[array.length - 1][0];
+  cArray[array.length + 1][0] = array[0][array[0].length - 1];
+  cArray[array.length + 1][array[0].length + 1] = array[0][0];
+
+  // Top and bottom
+  for (var j = 0; j < array[0].length; j++) {
+      cArray[0][j + 1] = array[array.length - 1][j];
+      cArray[array.length + 1][j + 1] = array[0][j];
+  }
+
+  // Left and right
+  for (var i = 0; i < array.length; i++) {
+    cArray[i + 1][0] = array[i][array[0].length - 1];
+    cArray[i + 1][array[0].length + 1] = array[i][0];
+  }
+
+  return cArray;
+}
+
 function copyArray(array) {
   cArray = [];
 
@@ -90,6 +145,7 @@ function copyArray(array) {
 
   return cArray;
 }
+
 function nextTick(array) {
   /* RULES
      1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
@@ -97,139 +153,46 @@ function nextTick(array) {
      3. Any live cell with more than three live neighbours dies, as if by overpopulation.
      4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
   */
-  cArray = copyArray(array);
+  var cArray = copyArray(array);
+  var array = increaseArray(array);
 
   // Rest
   for (var i = 1; i < array.length - 1; i++) {
     for (var j = 1; j < array[i].length - 1; j++) {
-      var aliveCells = array[i - 1][j - 1] + array[i - 1][j] + array[i - 1][j + 1] + array[i][j - 1] + array[i][j + 1] + array[i + 1][j - 1] + array[i + 1][j] + array[i + 1][j + 1];
-      if (!array[i][j]) {
-        // Dead cell
-        if (aliveCells == 3) cArray[i][j] = true;
-      } else {
-        // Alive cell
-        if (aliveCells < 2) cArray[i][j] = false;
-        else if (aliveCells == 2 || aliveCells == 3) cArray[i][j] = true;
-        else if (aliveCells > 3) cArray[i][j] = false;
+      var aliveCells = array[i - 1][j - 1] +
+                       array[i - 1][j] +
+                       array[i - 1][j + 1] +
+                       array[i][j - 1] +
+                       array[i][j + 1] +
+                       array[i + 1][j - 1] +
+                       array[i + 1][j] +
+                       array[i + 1][j + 1];
+
+      if (!array[i][j]) { // Dead cell
+        if (aliveCells == 3) cArray[i - 1][j - 1] = true;
+      } else { // Alive cell
+        if (aliveCells < 2) cArray[i - 1][j - 1] = false;
+        else if (aliveCells == 2 || aliveCells == 3) cArray[i - 1][j - 1] = true;
+        else if (aliveCells > 3) cArray[i - 1][j - 1] = false;
       }
     }
   }
 
-/*
-  // Left
-  for (var i = 1; i < array.length - 1; i++) {
-    var aliveCells = array[i - 1][array[0].length - 1] + array[i - 1][0] + array[i - 1][1] + array[i][array[0].length - 1] + array[i][1] + array[i + 1][array[0].length - 1] + array[i + 1][0] + array[i + 1][1];
-    if (!array[i][0]) {
-      // Dead cell
-      if (aliveCells == 3) cArray[i][0] = true;
-    } else {
-      // Alive cell
-      if (aliveCells < 2) cArray[i][0] = false;
-      else if (aliveCells == 2 || aliveCells == 3) cArray[i][0] = true;
-      else if (aliveCells > 3) cArray[i][0] = false;
-    }
-  }
-  // Right
-  for (var i = 1; i < array.length - 1; i++) {
-    var aliveCells = array[i - 1][array[0].length - 2] + array[i - 1][array[0].length - 1] + array[i - 1][0] + array[i][array[0].length - 2] + array[i][0] + array[i + 1][array[0].length - 2] + array[i + 1][array[0].length - 1] + array[i + 1][0];
-    if (!array[i][0]) {
-      // Dead cell
-      if (aliveCells == 3) cArray[i][0] = true;
-    } else {
-      // Alive cell
-      if (aliveCells < 2) cArray[i][0] = false;
-      else if (aliveCells == 2 || aliveCells == 3) cArray[i][0] = true;
-      else if (aliveCells > 3) cArray[i][0] = false;
-    }
-  }
-  // Top
-  for (var j = 1; j < array[0].length - 1; j++) {
-    var aliveCells = array[array.length - 1][j - 1] + array[array.length - 1][j] + array[array.length - 1][j + 1] + array[0][j - 1] + array[0][j + 1] + array[1][j - 1] + array[1][j] + array[1][j + 1];
-    if (!array[0][j]) {
-      // Dead cell
-      if (aliveCells == 3) cArray[0][j] = true;
-    } else {
-      // Alive cell
-      if (aliveCells < 2) cArray[0][j] = false;
-      else if (aliveCells == 2 || aliveCells == 3) cArray[0][j] = true;
-      else if (aliveCells > 3) cArray[0][j] = false;
-    }
-  }
-  // Bottom
-  for (var j = 1; j < array[0].length - 1; j++) {
-    var aliveCells = array[array.length - 2][j - 1] + array[array.length - 2][j] + array[array.length - 2][j + 1] + array[array.length - 1][j - 1] + array[array.length - 1][j + 1] + array[0][j - 1] + array[0][j] + array[0][j + 1];
-    if (!array[0][j]) {
-      // Dead cell
-      if (aliveCells == 3) cArray[0][j] = true;
-    } else {
-      // Alive cell
-      if (aliveCells < 2) cArray[0][j] = false;
-      else if (aliveCells == 2 || aliveCells == 3) cArray[0][j] = true;
-      else if (aliveCells > 3) cArray[0][j] = false;
-    }
-  }
-
-  // (0, 0)
-  var aliveCells = array[array.length - 1][array[0].length - 1] + array[array.length - 1][0] + array[array.length - 1][1] + array[0][array[0].length - 1] + array[0][1] + array[1][array[0].length - 1] + array[1][0] + array[1][1];
-  if (!array[0][0]) {
-    // Dead cell
-    if (aliveCells == 3) cArray[0][0] = true;
-  } else {
-    // Alive cell
-    if (aliveCells < 2) cArray[0][0] = false;
-    else if (aliveCells == 2 || aliveCells == 3) cArray[0][0] = true;
-    else if (aliveCells > 3) cArray[0][0] = false;
-  }
-
-  // (N, 0)
-  var aliveCells = array[array.length - 2][array[0].length - 1] + array[array.length - 2][0] + array[array.length - 2][1] + array[array.length - 1][array[0].length - 1] + array[array.length - 1][1] + array[0][array[0].length - 1] + array[0][0] + array[0][1];
-  if (!array[array.length - 1][0]) {
-    // Dead cell
-    if (aliveCells == 3) cArray[array.length - 1][0] = true;
-  } else {
-    // Alive cell
-    if (aliveCells < 2) cArray[array.length - 1][0] = false;
-    else if (aliveCells == 2 || aliveCells == 3) cArray[array.length - 1][0] = true;
-    else if (aliveCells > 3) cArray[array.length - 1][0] = false;
-  }
-
-  // (0, N)
-  var aliveCells = array[array.length - 1][array[0].length - 2] + array[array.length - 1][array[0].length - 1] + array[array.length - 1][0] + array[0][array[0].length - 2] + array[0][0] + array[1][array[0].length - 2] + array[1][array[0].length - 1] + array[1][0];
-  if (!array[0][array[0].length - 1]) {
-    // Dead cell
-    if (aliveCells == 3) cArray[0][array[0].length - 1] = true;
-  } else {
-    // Alive cell
-    if (aliveCells < 2) cArray[0][array[0].length - 1] = false;
-    else if (aliveCells == 2 || aliveCells == 3) cArray[0][array[0].length - 1] = true;
-    else if (aliveCells > 3) cArray[0][array[0].length - 1] = false;
-  }
-
-  // (N, N)
-  var aliveCells = array[array.length - 2][array[0].length - 2] + array[array.length - 2][array[i].length - 1] + array[array.length - 2][0] + array[array.length - 1][array[0].length - 2] + array[array.length - 1][0] + array[0][array[0].length - 2] + array[0][array[0].length - 1] + array[0][0];
-  if (!array[array.length - 1][array[0].length - 1]) {
-    // Dead cell
-    if (aliveCells == 3) cArray[array.length - 1][array[0].length - 1] = true;
-  } else {
-    // Alive cell
-    if (aliveCells < 2) cArray[array.length - 1][array[0].length - 1] = false;
-    else if (aliveCells == 2 || aliveCells == 3) cArray[array.length - 1][array[0].length - 1] = true;
-    else if (aliveCells > 3) cArray[array.length - 1][array[0].length - 1] = false;
-  }
-*/
-
   return cArray;
 }
+
 function gameLoop(timeStamp) {
-  setTimeout(() => {
-    drawGrid();
+  timeoutID = setTimeout(() => {
+    animationID = undefined;
 
     array = nextTick(array);
     drawArray(array);
+    drawGrid();
 
-    window.requestAnimationFrame(gameLoop)
+    start();
   }, 1000/fps);
 }
+
 function drawArray(array) {
   for (var i = 0; i < nRows; i++) {
     for (var j = 0; j < nCols; j++) {
@@ -241,6 +204,21 @@ function drawArray(array) {
         context.fillRect(j * width/nCols + 0.5, i * height/nRows + 0.5, width/nCols - 1.5, height/nRows - 1.5);
       }
     }
+  }
+}
+
+function start() {
+  if (!animationID) animationID = window.requestAnimationFrame(gameLoop);
+}
+
+function stop() {
+  if (animationID) {
+    window.cancelAnimationFrame(animationID);
+    animationID = undefined;
+  }
+
+  if (timeoutID) {
+    clearTimeout(timeoutID);
   }
 }
 
